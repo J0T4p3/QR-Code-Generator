2c35b89c4811532f130e8c193ddd02c1204d3726
João Pedro Silva <joaopedrosilvadelima@gmail.com>
2021-12-17 15:11:45 -0300

    Added Title and direct Download option

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
+let qrcodelink = select('a');
 let qrcode = select('img');
 let qrtext = select('textarea');
-let qrbtn = select('button');
+let qrbtn = select('#btnGenerate');
+let qrDownloadBtn = select('#btnDownload');
 
+//On page load, put a default qr code
 window.addEventListener('load', generateQR);
 
 qrbtn.addEventListener('click', generateQR);
+
+qrDownloadBtn.addEventListener('click', downloadQR);
 
 function generateQR() {
 	let size = '1000x1000';
 	let data = qrtext.value;
 	let baseURL = 'http://api.qrserver.com/v1/create-qr-code/';
 
+	//Default qr code
+	if (data == '') {
+		data = 'You got this! Now put your own text here.';
+	}
+
 	let url = `${baseURL}?data=${data}&size=${size}`;
 
 	qrcode.src = url;
+
+	//set the href of image to be the image
+	document.getElementById('DownloadImage').href = qrcode.src;
 }
+
+function downloadQR() {}
 
 function select(el) {
 	return document.querySelector(el);
